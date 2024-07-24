8df1281839c54e0b7f41474c3c36823373ccd010
Aleksandar Krastev <krastev.aleksander@gmail.com>
2024-07-24 17:34:06 +0300

    Add final test for basket

--- selenium/src/main/java/com/sashocompany/MyWebDriver.java
+++ selenium/src/main/java/com/sashocompany/MyWebDriver.java
@@ -1,14 +1,21 @@
 package com.sashocompany;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MyWebDriver {
 	private WebDriver driver;
+	private WebDriverWait wait;
+
 	private static String chromeDriverLocation = "C:\\chromedriver-win64\\chromedriver.exe";
 
 	MyWebDriver() {
@@ -18,25 +25,64 @@ public class MyWebDriver {
 		}
 		WebDriver webDriver = new ChromeDriver();
 
-		// Wait up to 1 second for element to appear.
+		// Wait up to 2 seconds for element to appear.
 		webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
 
 		webDriver.get("https://www.amazon.co.uk/");
 		this.driver = webDriver;
+		this.wait = new WebDriverWait(this.driver, Duration.ofSeconds(10));
 	}
 
-	// A constructor with webdriver name as parameter is possible for parameterization.
-	
+	// A constructor with webdriver name as parameter is possible for
+	// parameterization.
+
+	/**
+	 * Closes the driver.
+	 **/
 	public void closeDriver() {
 		this.driver.close();
 	}
-	
+
+	/**
+	 * The following serves double purpose: Exceptions are difficult to handle
+	 * especially in assertAll. At report level having 2 stacktraces - AssertError
+	 * and NoSuchElementException is ridiculous. On top it is easier to have a
+	 * meaningful message which includes some human readable information of the
+	 * element that you are looking for.
+	 * 
+	 * @param By Mechanism used to locate elements within a document.
+	 * @return null or the element matching the By criteria
+	 **/
 	public WebElement findElement(By by) {
-		return this.driver.findElement(by);
+		try {
+			return this.driver.findElement(by);
+		} catch (NoSuchElementException e) {
+			return null;
+		}
 	}
 	
+	public List<WebElement> findElements(By by) {
+		return this.driver.findElements(by);
+	}
+	
+	public void click(By by) {
+	    Actions actions = new Actions(this.driver);
+	    wait.until(ExpectedConditions.elementToBeClickable(by));
+	    WebElement element = findElement(by);
+	    actions.moveToElement(element).click().build().perform();
+	}
+
+	/**
+	 * @return current URL
+	 **/
 	public String getURL() {
 		return this.driver.getCurrentUrl();
 	}
-		
+
+	/**
+	 * @param url URL to navigate to.
+	 **/
+	public void navigateToUrl(String url) {
+		this.driver.navigate().to(url);
+	}
 }
--- selenium/src/test/java/com/sashocompany/AmazonUKPageTest.java
+++ selenium/src/test/java/com/sashocompany/AmazonUKPageTest.java
@@ -1,137 +1,35 @@
 package com.sashocompany;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 @TestInstance(Lifecycle.PER_CLASS)
-class AmazonUKPageTest {
-
-	private MyWebDriver myWebDriver;
-	private static String FIRST_SEARCH_RESULT_XPATH = "//div[@data-cel-widget='search_result_1']";
-	private static String FIRST_SEARCH_RESULT_TITLE_XPATH = FIRST_SEARCH_RESULT_XPATH
-			+ "//span[@class='a-size-medium a-color-base a-text-normal']";
-	private static String FIRST_SEARCH_RESULT_TYPE_XPATH = FIRST_SEARCH_RESULT_XPATH + "//*[text()='%s']";
-	private static String FIRST_SEARCH_RESULT_PRICE_XPATH = "(" + FIRST_SEARCH_RESULT_TYPE_XPATH
-			+ "/following::div[1]//span[@class='a-offscreen'])[1]";
-	private static String PRODUCT_TITLE_XPATH = "//*[@id='productTitle']";
-	private static String FOCUSED_COVER_TYPE_XPATH = "//*[@id='tmm-grid-swatch-PAPERBACK' and contains(@class, 'selected')]";
-	private static String FOCUSED_COVER_TYPE_NAME_XPATH = FOCUSED_COVER_TYPE_XPATH + "//a/span[1]/span";
-	private static String FOCUSED_COVER_TYPE_PRICE_XPATH = FOCUSED_COVER_TYPE_XPATH + "//a/span[2]/span";
-	private static List<String> typesOfCover = Arrays.asList("Paperback", "Hardcover",
-			"Kindle Edition with Audio/Video");
-
-	@BeforeAll
-	void initializeDriver() {
-		this.myWebDriver = new MyWebDriver();
-	}
-
-	@AfterAll
-	void clozeDriver() {
-		this.myWebDriver.closeDriver();
-	}
-
-	public void assertFirstResult(String title) {
-		WebElement firstSearchResult = this.myWebDriver.findElement(By.xpath(FIRST_SEARCH_RESULT_XPATH));
-		WebElement firstSearchResultTitle = this.myWebDriver.findElement(By.xpath(FIRST_SEARCH_RESULT_TITLE_XPATH));
-
-		/*
-		 * I could probably add even more assertions for presence of picture, whether it
-		 * says "Instant delivery" under Kindle Edition, whether the delivery date is in
-		 * the future and delivery is to the country where the calls are made from. But
-		 * all of those should be added below so that the execution doesn't stop on the
-		 * first fail OR new tests are added.
-		 */
-		assertAll("Grouped Assertions of Search result",
-				() -> assertNotNull(firstSearchResult, "There aren't any search results."),
-				() -> assertNotNull(firstSearchResultTitle, "There aren't any titles of the first search result."),
-				() -> assertTrue(firstSearchResultTitle.getAttribute("innerHTML").startsWith(title),
-						"There first result title doesn't match the search."));
-
-		/*
-		 * I added check for the cover types. I didn't find a way to include the
-		 * following checks in the assertAll. I tried () -> { ... } but only the first
-		 * assert in the ... was checked. I also tried individually listing the elements
-		 * for the different covers but the code was too repetitive.
-		 */
-		for (String cover : typesOfCover) {
-			WebElement coverElement = this.myWebDriver
-					.findElement(By.xpath(String.format(FIRST_SEARCH_RESULT_TYPE_XPATH, cover)));
-			WebElement coverPrice = this.myWebDriver
-					.findElement(By.xpath(String.format(FIRST_SEARCH_RESULT_PRICE_XPATH, cover)));
-			assertNotNull(coverElement, "There aren't any " + cover + " copies for the search result.");
-			assertNotNull(coverPrice, "There isn't a price for the " + cover + " copy.");
-		}
-
-	}
-
-	private void assertTitleAndPrice(String title, HashMap<String, String> typesOfCoverAndPrices) {
-		WebElement productTitle = this.myWebDriver.findElement(By.xpath(PRODUCT_TITLE_XPATH));
-		WebElement focusedCoverType = this.myWebDriver.findElement(By.xpath(FOCUSED_COVER_TYPE_NAME_XPATH));
-		WebElement focusedCoverTypePrice = this.myWebDriver.findElement(By.xpath(FOCUSED_COVER_TYPE_PRICE_XPATH));
-
-		assertAll("Grouped Assertions of Product", () -> assertNotNull(productTitle, "There isn't a Product Title"),
-				() -> assertTrue(productTitle.getAttribute("innerHTML").contains(title),
-						"There product title " + productTitle.getAttribute("innerHTML") + " doesn't match the search."),
-				() -> assertNotNull(focusedCoverType, "There isn't a focused cover type"),
-				() -> assertEquals("Paperback", focusedCoverType.getAttribute("innerHTML"),
-						"There focused cover type is not Paperback."),
-				() -> assertNotNull(focusedCoverTypePrice, "There isn't a focused cover type price"),
-				// For some weird reason there are intervals before and after the price.
-				() -> assertEquals(" " + typesOfCoverAndPrices.get("Paperback") + " ",
-						focusedCoverTypePrice.getAttribute("innerHTML")));
-	}
-
-	private void clickFirstPaperbackResult() {
-		WebElement paperBack = this.myWebDriver
-				.findElement(By.xpath(String.format(FIRST_SEARCH_RESULT_TYPE_XPATH, "Paperback")));
-		paperBack.click();
-	}
-
-	private HashMap<String, String> getPricesOfPaperBackHardCoverAndKindleEdition() {
-		HashMap<String, String> typesOfCoverAndPrices = new HashMap<>();
-		for (String cover : typesOfCover) {
-			String priceString = this.myWebDriver
-					.findElement(By.xpath(String.format(FIRST_SEARCH_RESULT_PRICE_XPATH, cover)))
-					.getAttribute("innerHTML");
-			assertTrue(priceString.matches("^£\\d+[\\.,]\\d{2}"),
-					"There price " + priceString + " for the " + cover + " doesn't match the pattern.");
-			typesOfCoverAndPrices.put(cover, priceString);
-		}
-
-		return typesOfCoverAndPrices;
-	}
-
-	public void searchFor(String searchable) {
-		WebElement searchBox = this.myWebDriver.findElement(By.id("twotabsearchtextbox"));
-		searchBox.sendKeys(searchable);
-		WebElement searchButton = this.myWebDriver.findElement(By.id("nav-search-submit-button"));
-		searchButton.click();
-	}
+class AmazonUKPageTest extends AmazonUKPageBase {
 
 	@ParameterizedTest
 	@ValueSource(strings = { "Harry Potter and the Cursed Child - Parts One and Two" })
 	void whenSearchForAnItemThenItShowsResultWithAllAvailableCopyOptions(String title) {
 		searchFor(title);
 		assertFirstResult(title);
+		/*
+		 * #AdditionalChecks
+		 * Assert for presence of picture, whether it
+		 * says "Instant delivery" under Kindle Edition, whether the delivery date is in
+		 * the future and delivery is to the country where the calls are made from. But
+		 * all of those should be added in assertAll so that the execution doesn't stop on the
+		 * first fail OR new tests are added.
+		 */
 		HashMap<String, String> typesOfCoverAndPrices = getPricesOfPaperBackHardCoverAndKindleEdition();
 		clickFirstPaperbackResult();
 		assertTitleAndPrice(title, typesOfCoverAndPrices);
+		// #AdditionalChecks similar checks can be added for the cover types , Hardcover and
+		// KindleEdition.
 	}
 
 	@ParameterizedTest
@@ -139,27 +37,32 @@ class AmazonUKPageTest {
 	@ValueSource(strings = { "Harry Potter and the Cursed Child - Parts One and Two" })
 	void whenNavigateToTheFirstResultForAnItemThenItShowsProperContent(String title) {
 		/*
-		 * This test shows that the navigating to
+		 * #AdditionalChecks
+		 * This tests whether navigating to
 		 * "https://www.amazon.co.uk/Harry-Potter-Cursed-Child-Playscript/dp/0751565369/
 		 * ref=sr_1_1?keywords=Harry+Potter+and+the+Cursed+Child+-+Parts+One+and+Two"
-		 * will lead to a page that has the following information: 
-		 * - Title, 
-		 * - Prices for the three cover types - Paperback, Hardcover and Kindle Edition
-		 * - Add to Basket button
-		 * - Buy Now button
-		 * - Author(s)
-		 * - Frequently bought together
-		 * - Popular titles by this author
-		 * - Customers who viewed this item also viewed
-		 * - Product description
-		 * - Product details
-		 * - About the authors
-		 * - Products related to this item
-		 * - Customer reviews
-		 * - Customers who bought this item also bought
-		 * - Customers who viewed items in your browsing history also viewed
-		 * - Related to items you've viewed
+		 * will lead to a page that has the following information: Title, Prices for the
+		 * three cover types, Paperback, Hardcover and Kindle Edition, Add to Basket
+		 * button, Buy Now button, Author(s), Frequently bought together, Popular titles
+		 * by this author, Customers who viewed this item also viewed, Product
+		 * description, Product details, About the authors, Products related to this
+		 * item, Customer reviews, Customers who bought this item also bought, Customers
+		 * who viewed items in your browsing history also viewed, Related to items
+		 * you've viewed.
 		 */
 	}
 
+	@ParameterizedTest
+	@CsvSource({
+			"https://www.amazon.co.uk/Harry-Potter-Cursed-Child-Playscript/dp/0751565369/ref=sr_1_1?keywords=Harry+Potter+and+the+Cursed+Child+-+Parts+One+and+Two, Harry Potter and the Cursed Child - Parts One and Two" })
+	void whenAddItemToTheBasketAndNavigateToTheBasketThenItIsPresentThere(String url, String title) {
+		navigateToUrl(url);
+		String paperbackPrice = getPaperbackPrice();
+		addToBasket();
+		// #AdditionalChecks Automatically redirected to 'Added to Basket' screen. 
+		navigateToBasket();
+		assertBasket(title, paperbackPrice);
+		// #AdditionalChecks Quantity is 1, Delete, Save for Later, See more like, Share links are available. 
+		clickThisWillBeAGift();
+	}
 }
